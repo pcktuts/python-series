5175131867210f405d21d19369cdfa2bcffabf9d
Learn Programming In Malayalam <63596426+pcktuts@users.noreply.github.com>
2020-04-16 23:20:15 +0530

    Create 10-for-loops.py

---
+++ 10-for-loops.py
@@ -0,0 +1,28 @@
+# For loops
+
+mylist = ["a", "b", "c", "d"]
+
+# for x in mylist:
+#     print(x)
+#     if x == "c":
+#         break
+
+# for x in mylist:    
+#     if x == "b":
+#         continue
+#     print(x)
+
+# for x in range(10, 20, 2): 
+#     print(x)
+# else:
+#     print("Loop completed")
+
+# Nested loops
+a = ["a", "b", "c"]
+b = ["d", "e"]
+
+# for x in a:
+#     for y in b:
+#         print(x, y)
+for x in a:
+    pass
