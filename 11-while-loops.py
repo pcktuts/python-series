a705e850c68c6622177976bf473847ef008cb076
Learn Programming In Malayalam <63596426+pcktuts@users.noreply.github.com>
2020-04-17 14:33:25 +0530

    Create 11-while-loops.py

---
+++ 11-while-loops.py
@@ -0,0 +1,13 @@
+# While Loops 
+x = 1
+# while x < 10:
+#     x +=1
+#     if x == 3:
+#         continue
+#     print(x)
+# else:
+#     print("While loop ended")
+    
+# Infinite loop
+while x == 1:
+    print("hi")
