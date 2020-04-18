1cfc609994c485b380f794e22c0bcef79e517478
Learn Programming In Malayalam <63596426+pcktuts@users.noreply.github.com>
2020-04-18 22:33:14 +0530

    Create 14-recursion.py

---
+++ 14-recursion.py
@@ -0,0 +1,11 @@
+# Recursive function
+def factorial(number):
+    if number == 1:
+        return number
+    return number * factorial(number-1)
+print(factorial(5))
+# 5 * factorial(4)
+# 5 * 4 * factorial(3)
+# 5 * 4 * 3 * factorial(2)
+# 5 * 4 * 3 * 2 * factorial(1)
+# 5 * 4 * 3 * 2 * 1
