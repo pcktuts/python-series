e5d982758539d84e7c4f4d66ed1e1a8c638cf8ec
Learn Programming In Malayalam <63596426+pcktuts@users.noreply.github.com>
2020-04-25 13:07:36 +0530

    Create main.py

---
+++ 25/main.py
@@ -0,0 +1,10 @@
+# Modules 
+# import mymodule as my
+# from mymodule import student
+# # s = my.Hello()
+# # print(s.say_hi())
+# print(student)
+
+# inbuilt modules
+import platform 
+print(platform.system() )
