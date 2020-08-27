491c955987d03c061b51970d86d51757f3a1536b
Learn Programming In Malayalam <63596426+pcktuts@users.noreply.github.com>
2020-08-27 17:01:57 +0530

    Create main.py

---
+++ 41/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, url_for, request
+app = Flask(__name__)
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="4b3#U>6@S^SLeJn",
+    database="learn_sql"
+)
+
+mycursor = mydb.cursor(dictionary=True)
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':   
+        return render_template('login.html')
+
+    email = request.form['email']
+    password = request.form['pwd']
+    print(email, password)        
+    sql = "SELECT * FROM learn_sql.users WHERE email= '"+ email +"' AND password = '"+ password +"';"
+    print(sql)
+    mycursor.execute(sql)
+    myresult = mycursor.fetchone()
+    mydb.commit()
+    print(mycursor.rowcount)
+    if mycursor.rowcount > 0:
+        return "login success"
+
+    return "login failed"
+    
+
